8c605137aac45339b868b933d9b9ad26d9aff2b4
thecolab <nomadone@Nurs-MacBook-Pro.local>
2014-08-15 16:25:40 +0200

    restore old thinking page
    save asif’s tabs changes and rename and restore earlier thinking page

--- javascript/cbpAnimatedHeader.js
+++ javascript/cbpAnimatedHeader.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
-                    $('#header-21 .dropdown').find(".dropdown-menu").each(function() {
-                      $(this).addClass("subnav");
-                      $(this).removeClass("dropdown-menu");
-                    });
-                  });
+	$('#header-21 .dropdown').find(".dropdown-menu").each(function() {
+	  $(this).addClass("subnav");
+	  $(this).removeClass("dropdown-menu");
+	});
+  });
